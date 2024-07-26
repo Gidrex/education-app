dde124351e14288744a51d140506df302ded8827
Alexander <Desench@proton.me>
2024-07-26 15:53:36 +0300

    1

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -8,11 +8,56 @@ class FestivalScheduleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Festival Schedule'),
-        ),
-        body: FestivalSchedulePage(),
+      home: FestivalHomePage(),
+    );
+  }
+}
+
+class FestivalHomePage extends StatefulWidget {
+  @override
+  _FestivalHomePageState createState() => _FestivalHomePageState();
+}
+
+class _FestivalHomePageState extends State<FestivalHomePage> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    FestivalSchedulePage(),
+    Text('Главная', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+    Text('Обучение', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Festival Schedule'),
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.schedule),
+            label: 'Расписание',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Главная',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Обучение',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
@@ -38,7 +83,76 @@ class FestivalSchedulePage extends StatelessWidget {
           speaker: "Сообщества РЖА",
           color: Colors.purple[100]!,
         ),
-        // Добавь остальные секции согласно изображению
+        ScheduleSection(
+          time: "12:30-13:00",
+          title: "Лекция: 'Что такое семья'",
+          location: "Лекторий 2",
+          speaker: "Клара Бернаскони",
+          color: Colors.purple[200]!,
+        ),
+        ScheduleSection(
+          time: "12:30-13:00",
+          title: "Зона аниматоров РЖА",
+          location: "Общая зона 'Оpen space'",
+          speaker: "",
+          color: Colors.purple[100]!,
+        ),
+        ScheduleSection(
+          time: "13:00-14:00",
+          title: "Лекция: 'Что такое реклама и PR'",
+          location: "Лекторий 1",
+          speaker: "Михаил Иванов",
+          color: Colors.purple[200]!,
+        ),
+        ScheduleSection(
+          time: "13:00-14:00",
+          title: "Лекция",
+          location: "Секция 3",
+          speaker: "",
+          color: Colors.purple[100]!,
+        ),
+        ScheduleSection(
+          time: "13:00-13:30",
+          title: "Лекция: 'Что такое реклама и PR'",
+          location: "Лекторий 2",
+          speaker: "",
+          color: Colors.purple[200]!,
+        ),
+        ScheduleSection(
+          time: "13:00-13:30",
+          title: "Лекция",
+          location: "Секция 4",
+          speaker: "",
+          color: Colors.purple[100]!,
+        ),
+        ScheduleSection(
+          time: "13:30-14:00",
+          title: "Общий фуршет",
+          location: "Кухня",
+          speaker: "",
+          color: Colors.purple[200]!,
+        ),
+        ScheduleSection(
+          time: "14:00-14:30",
+          title: "Лекция: 'Что такое семья'",
+          location: "Лекторий 1",
+          speaker: "Дарья Чайковская",
+          color: Colors.purple[100]!,
+        ),
+        ScheduleSection(
+          time: "14:30-15:00",
+          title: "Пение на жестовом языке",
+          location: "Лекторий 1",
+          speaker: "",
+          color: Colors.purple[200]!,
+        ),
+        ScheduleSection(
+          time: "15:00-15:30",
+          title: "Театральные постановки на РЖА",
+          location: "Лекторий 1",
+          speaker: "",
+          color: Colors.purple[100]!,
+        ),
         ScheduleSection(
           time: "15:30-16:00",
           title: "Закрытие фестиваля",
@@ -68,31 +182,28 @@ class ScheduleSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: color,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              time,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 4),
-            Text(
-              title,
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 4),
-            Text('Место: $location'),
-            if (speaker.isNotEmpty) ...[
-              SizedBox(height: 4),
-              Text('Спикер: $speaker'),
-            ],
-          ],
-        ),
+    return ExpansionTile(
+      leading: Icon(Icons.arrow_downward),
+      title: Text(
+        time,
+        style: TextStyle(fontWeight: FontWeight.bold),
       ),
+      subtitle: Text(title),
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Место: $location'),
+              if (speaker.isNotEmpty) ...[
+                SizedBox(height: 4),
+                Text('Спикер: $speaker'),
+              ],
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
