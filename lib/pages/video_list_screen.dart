964b9cdea8c85b866fb98b395399a7d60918ff89
Alexander <Desench@proton.me>
2024-09-08 15:26:41 +0300

    Alexey doing good

--- lib/pages/video_list_screen.dart
+++ lib/pages/video_list_screen.dart
@@ -5,19 +5,23 @@ class VideoListScreen extends StatelessWidget {
   final List<Map<String, String>> videos = [
     {
       'title': 'Урок 1',
-      'videoUrl': 'https://mega.nz/embed/zYsDRAyb#HfyhfCNQbTZRrhMNZ6pzhzgJnCQ7YtunGj3978UECRQ', // Прямая ссылка на видео
+      'videoUrl': 'https://mega.nz/embed/zYsDRAyb#HfyhfCNQbTZRrhMNZ6pzhzgJnCQ7YtunGj3978UECRQ',
+      'posterUrl': 'lib/assets/poster1.jpg',
     },
     {
       'title': 'Урок 2',
       'videoUrl': 'https://mega.nz/embed/aIFnTAYb#SNaTWL4W1Odw8Q_b7JN5dm9LJ4TaHEfsMo22X4vq3_8',
+      'posterUrl': 'lib/assets/poster2.jpg',
     },
     {
       'title': 'Урок 3',
       'videoUrl': 'https://mega.nz/embed/2ZEH3ILb#EXOL7ZU4BUUZUg7z6MzLEZzPwVy4WpTcIi8nrPXhfgo',
+      'posterUrl': 'lib/assets/poster3.jpg',
     },
     {
       'title': 'Урок 4',
       'videoUrl': 'https://mega.nz/embed/rAMhXLgL#W4CXIz0dg8C4tWnAxfGa7HdopTWmkCHxnEEyJl_BGUQ',
+      'posterUrl': 'lib/assets/poster4.jpg',
     },
   ];
 
@@ -50,18 +54,29 @@ class VideoListScreen extends StatelessWidget {
                         ),
                       );
                     },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.black12,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Center(
-                        child: Icon(Icons.play_arrow, size: 50, color: Colors.white),
-                      ),
+                    child: Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            image: DecorationImage(
+                              image: AssetImage(video['posterUrl'] ?? 'assets/default_poster.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Center(
+                          child: Icon(
+                            Icons.play_circle_outline,
+                            size: 80.0,
+                            color: Colors.white.withOpacity(0.8),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-                SizedBox(height: 8), // Отступ между видео и заголовком
+                SizedBox(height: 8),
                 Text(
                   video['title'] ?? 'Без названия',
                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
